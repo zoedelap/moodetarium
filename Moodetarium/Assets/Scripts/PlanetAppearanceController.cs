55194fdb70f3a88d014f0529ef1957ba70058975
zoedelap <zoe.a.delap@gmail.com>
2023-04-01 21:24:20 -0500

    materials, change colors on spacebar push, turning planets

---
+++ Moodetarium/Assets/Scripts/PlanetAppearanceController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetAppearanceController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            setColor();
+        }
+    }
+
+    void setColor() {
+        // Get the Renderer component from the new cube
+       var renderer = GetComponent<Renderer>();
+
+        // Create a new RGBA color using the Color constructor and store it in a variable
+        Color customColor = new Color(0.4f, 0.9f, 0.7f, 1.0f);
+
+       // Call SetColor using the shader property name "_Color" and setting the color to the custom color you created
+       renderer.material.SetColor("_Color", customColor);
+    }
+}
