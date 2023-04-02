b75714630ed31ba454070567bc1589197ecbe091
zoedelap <zoe.a.delap@gmail.com>
2023-04-01 22:29:08 -0500

    colors scale by the responses now

--- Moodetarium/Assets/Scripts/PlanetManager.cs
+++ Moodetarium/Assets/Scripts/PlanetManager.cs
@@ -24,10 +24,16 @@ public class PlanetManager : MonoBehaviour
     }
 
     Color convertColor(float num) {
-        if (num <= 2.5) {
-            return Color.red;
+        if (num < 1) {
+            return Color.grey;
+        } else if (num < 2) {
+            return new Color(0.07f, 0.23f, 0.54f, 1.0f);
+        } else if (num < 3) {
+            return new Color(0.40f, 0.07f, 0.66f, 1.0f);
+        } else if (num < 4) {
+            return new Color(0.94f, 0.00f, 0.30f, 1.0f);
         } else {
-            return Color.blue;
+            return new Color(0.94f, 0.73f, 0.00f, 1.0f);
         }
     }
 }
